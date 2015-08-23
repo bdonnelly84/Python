7a2ef2c7a0e37971fa42b285b09d5ae552a830d5
Ben Donnelly <ben.donnelly.84@gmail.com>
2015-08-23 18:34:40 +1000

    Update chap8.py
    Added solution for Exercise 1.

--- python_for_informatics/chap8.py
+++ python_for_informatics/chap8.py
@@ -1,1 +1,11 @@
+""" This program is for Exercises 1 of Chapter 8 """
 
+def chop(list):
+	length = len(list)
+	del list[length-1]
+	del list[0]
+
+def middle(list):
+	length = len(list)
+	answer = list[1:length-1]
+	return answer
